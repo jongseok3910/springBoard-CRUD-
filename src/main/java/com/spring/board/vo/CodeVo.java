ac1f6b89b38237844d3c140017da98b666d366e3
jongseok3910 <jongseok3910@gmail.com>
2021-06-26 22:51:49 +0900

    .

---
+++ src/main/java/com/spring/board/vo/CodeVo.java
@@ -0,0 +1,54 @@
+package com.spring.board.vo;
+
+public class CodeVo {
+	private String codetype;
+	private String codeId;
+	private String codeName;
+	private String creator;
+	private String creatorTime;
+	private String modifier;
+	private String modifiedTime;
+	public String getCodetype() {
+		return codetype;
+	}
+	public void setCodetype(String codetype) {
+		this.codetype = codetype;
+	}
+	public String getCodeId() {
+		return codeId;
+	}
+	public void setCodeId(String codeId) {
+		this.codeId = codeId;
+	}
+	public String getCodeName() {
+		return codeName;
+	}
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getCreatorTime() {
+		return creatorTime;
+	}
+	public void setCreatorTime(String creatorTime) {
+		this.creatorTime = creatorTime;
+	}
+	public String getModifier() {
+		return modifier;
+	}
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+	public String getModifiedTime() {
+		return modifiedTime;
+	}
+	public void setModifiedTime(String modifiedTime) {
+		this.modifiedTime = modifiedTime;
+	}
+	
+}
